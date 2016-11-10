1b5a491369296681bbd661436c414133341a276b
I322206 <I322206@INLN50899740A.apj.global.corp.sap>
2016-11-10 15:41:30 +0530

    change made to main class

--- JavaBegins/src/practice/Main.java
+++ JavaBegins/src/practice/Main.java
@@ -4,7 +4,11 @@ public class Main {
 	public static void main(String[] args){
 		Employee ref = new Employee();
 		ref.display();
+		displayMe();
 		
+	}
+	private static void displayMe(){
+		System.out.println("private method of main class is accessed");
 	}
 
 }
